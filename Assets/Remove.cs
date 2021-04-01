d4c0a139ddeee3963be95da9a3fb5bdf5466dd66
Sheepsers <tildenhsmith@gmail.com>
2021-04-01 18:26:27 -0400

    Full Demo Complete

---
+++ Assets/Remove.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Remove : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        Invoke("RemoveText", 2f);
+    }
+
+    private void RemoveText()
+    {
+        this.gameObject.SetActive(false);
+    }
+}
